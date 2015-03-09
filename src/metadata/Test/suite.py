bbabcf4d64e193b23b1703ce3c1bedbc31d6b270
Chris Loew <cloew123@gmail.com>
2015-03-08 22:32:04 -0600

    Added new metadata suite that includes general metadata helpers

--- src/metadata/Test/suite.py
+++ src/metadata/Test/suite.py
@@ -1,6 +1,8 @@
 import unittest
 
+from metadata.general.Test.suite import suite as general_suite
 from metadata.python.Test.suite import suite as python_suite
 
-suites = [python_suite]
+suites = [python_suite,
+          general_suite]
 suite = unittest.TestSuite(suites)
