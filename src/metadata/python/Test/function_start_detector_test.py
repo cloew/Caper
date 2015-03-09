759b2548b91bb14e59492ac6a3ee0d7c98c1c999
Chris Loew <cloew123@gmail.com>
2015-03-08 22:32:36 -0600

    Updated function start detector so it uses a File Line

--- src/metadata/python/Test/function_start_detector_test.py
+++ src/metadata/python/Test/function_start_detector_test.py
@@ -1,12 +1,13 @@
 import unittest
 
+from metadata.general.file_line import FileLine
 from metadata.python.function_start_detector import FunctionStartDetector
 
 class isStart(unittest.TestCase):
     """ Test cases of isStart """
-    FUNC_START = "def "
-    FUNC_START_WHITESPACE = "\tdef "
-    NOT_FUNC_START = ""
+    FUNC_START = FileLine(0, ["def "])
+    FUNC_START_WHITESPACE = FileLine(0, ["\tdef "])
+    NOT_FUNC_START = FileLine(0, [""])
     
     def setUp(self):
         """ Build the detector for the test """
