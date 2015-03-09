9ec75414de378c4a3550bb4bbda1523119635538
Chris Loew <cloew123@gmail.com>
2015-03-08 22:54:33 -0600

    Added helper class to represent the Leading Whitespace of a line in a Python file

--- src/metadata/python/Test/suite.py
+++ src/metadata/python/Test/suite.py
@@ -1,8 +1,10 @@
 import unittest
 
+from metadata.python.Test.leading_whitespace_test import suite as leading_whitespace_suite
 from metadata.python.Test.whitespace_helper_test import suite as whitespace_helper_suite
 from metadata.python.Test.function_start_detector_test import suite as function_start_detector_suite
 
 suites = [function_start_detector_suite,
-          whitespace_helper_suite]
+          whitespace_helper_suite,
+          leading_whitespace_suite]
 suite = unittest.TestSuite(suites)
---
+++ src/metadata/python/leading_whitespace.py
@@ -0,0 +1,19 @@
+
+class LeadingWhitespace:
+    """ Represents the leading whitespace for a line in a Python file """
+    
+    def __init__(self, line):
+        """ Initialize with the line to act as """
+        self.whitespace = line[:len(line)-len(line.lstrip())]
+        
+    def __len__(self):
+        """ Return the length of the leading whitespace """
+        return len(self.whitespace)
+        
+    def __lt__(self, other):
+        """ Return if this leading whitespace is less than the other """
+        return len(self) < len(other)
+        
+    def __eq__(self, other):
+        """ Return if this leading whitespace is equal to the other """
+        return len(self) == len(other)
